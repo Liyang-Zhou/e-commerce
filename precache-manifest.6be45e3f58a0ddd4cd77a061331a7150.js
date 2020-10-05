8622750bd81cc44090df8839e8c1e8475a7bcde3
liyang zhou <liyangzhou@liyangs-MacBook-Pro.local>
2020-10-05 00:36:56 -0700

    Updates

---
+++ precache-manifest.6be45e3f58a0ddd4cd77a061331a7150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda9fcb71cb001193c3f89c4463837eb",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "5c5d97408d8c38a42ab2",
+    "url": "/e-commerce/static/css/main.c73e252c.chunk.css"
+  },
+  {
+    "revision": "f22aadb78b191439bc7b",
+    "url": "/e-commerce/static/js/2.574e3021.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/e-commerce/static/js/2.574e3021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d97408d8c38a42ab2",
+    "url": "/e-commerce/static/js/main.9d6a7dc1.chunk.js"
+  },
+  {
+    "revision": "ff3875532917d432b871",
+    "url": "/e-commerce/static/js/runtime-main.2052b0b3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce/static/media/crown.ed28accb.svg"
+  }
+]);
